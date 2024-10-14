34c2855d6b4c6fd762e13e29884f7f6c0b2052ef
Hanna Lakus <annalakus@ukr.net>
2024-10-14 22:30:52 +0100

    start node

---
+++ src/constants/contacts.js
@@ -0,0 +1,10 @@
+import path from 'node:path';
+
+// console.log(path);
+// console.log(path.dirname);
+
+// console.log(process.cwd());
+
+export const PATH_DB = path.join(process.cwd(), 'src', 'constants', 'contacts');
+
+// console.log(PATH_DB, 'PATH_DB');
